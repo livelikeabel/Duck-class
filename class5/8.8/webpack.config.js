7ad30363af1ca04c856401835a90837ee0713714
livelikeabel <esung1129@gmail.com>
2018-08-08 22:54:14 +0900

    class5

---
+++ class5/8.8/webpack.config.js
@@ -0,0 +1,29 @@
+console.log(process.env.NODE_ENV)
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  mode: process.env.NODE_ENV == 'development' ? 'development' : 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    })
+  ],
+  devServer: {
+    // contentBase: path.join(__dirname, 'public'),
+    contentBase: [path.join(__dirname, 'public'), path.join(__dirname, 'assets')]
+    // publicPath: '/blog/'
+  }
+}
